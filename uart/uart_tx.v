6dddad77e99e5129bd906757f65c2bb34aaf83a9
Ahmet Ekrem Akel <111308260+AhmetEkremAkel@users.noreply.github.com>
2025-05-01 22:07:50 +0300

    Update uart_tx.v

--- uart/uart_tx.v
+++ uart/uart_tx.v
@@ -26,7 +26,7 @@ module uart_tx #(
     reg timertick,timerrun;
     reg [1:0] state;
     reg [BIT_COUNT-1:0] shreg;
-    reg [$clog2(BIT_COUNT)-1:0] bitcounter;
+    integer bitcounter;
 
 
     always @(posedge clk) begin
@@ -38,56 +38,61 @@ module uart_tx #(
             if (tx_start_i == 1) begin
                 shreg            <= din_i; 
                 state            <= S_START;
+                timerrun         <= 1;
             end
         end
 
         S_START: begin
                 tx_o             <= 0;
-                timerrun         <= 1;
                 bitcounter       <= 0;
                 if(timertick == 1)begin
-                shreg            <= din_i;
                 state            <= S_DATA;
+                tx_o <= shreg[0];
                 timerrun <= 1;
+                timertick <= 0;
                 end
         end
 
         S_DATA: begin
-            tx_o <= shreg[bitcounter];
-            if (bitcounter < BIT_COUNT) begin
-                
-                if(timertick == 1)begin
-                shreg[BIT_COUNT-2:0] <= shreg[BIT_COUNT-1:1];
-                bitcounter  <= bitcounter + 1;
-                timerrun    <= 1;
-                timertick   <= 0;
-                end
-            end
-            else if(bitcounter == BIT_COUNT-1)begin
+            tx_o <= shreg[0];
+            if(bitcounter == BIT_COUNT ) begin
+                tx_o <= 1;
                 state <= S_STOP;
+                timerrun <= 1;
+                timertick <= 0;
+            end else begin
+                if (timertick == 1) begin
+                    bitcounter <= bitcounter + 1;
+                    shreg[6:0] <= shreg[7:1];
+                    tx_o <= shreg[0];
+                    timertick <= 0;
+                    timerrun <= 1;
+                end
             end
         end
 
         S_STOP: begin
             tx_o <= 1;
-            timerrun <= 1;
-            if(timerrun == 1)begin
-            tx_done_tick_o <= 1;
-            state <= S_IDLE;
+            if(timertick == 1)begin
+                tx_done_tick_o <= 1;
+                state <= S_IDLE;
+                timertick   <= 0;
             end
         end
         default: state <= S_IDLE;
     endcase
+
     if(timerrun == 1) begin
-    timertick       <= 0;
-    timercount      <= 0;
-      if(timercount == bittimer)begin
-        timerrun    <= 0;
-        timercount  <= 0;
-        timertick   <= 1;
-      end
-      else timercount <= timercount + 1;
-      end
+        timertick       <= 0;
+        timercount      <= 0;
+        if(timercount == bittimer)begin
+            timerrun    <= 0;
+            timercount  <= 0;
+            timertick   <= 1;
+        end
+        else timercount <= timercount + 1;
+    end
+
 end
 
 
